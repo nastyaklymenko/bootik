f1aa2c37d5886a196dbe3f7462e04afb2200b2b3
Oleksii Dzhus <oleksa777@live.com>
2018-07-06 10:58:05 +0300

    Fully functioned version created

---
+++ src/main/java/com/example/creators/ActivityCreator.java
@@ -0,0 +1,36 @@
+package com.example.creators;
+
+import com.example.spellcheckers.JazzySpellChecker;
+import com.microsoft.bot.schema.models.Activity;
+import com.microsoft.bot.schema.models.ActivityTypes;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActivityCreator {
+
+  private static final String spellCheckedResponsePart = "You have probably meant: ";
+  private static final String echoResponsePart = "You typed: ";
+  private static JazzySpellChecker spellChecker = new JazzySpellChecker();
+
+  private ActivityCreator() {
+
+  }
+
+  public static Activity createSpellCheckedActivity(Activity activity) {
+    return createEmptyActivity(activity)
+        .withText(spellCheckedResponsePart + spellChecker.getCorrectedText(activity.text()));
+  }
+
+  public static Activity createEchoActivity(Activity activity) {
+    return createEmptyActivity(activity)
+        .withText(echoResponsePart + activity.text());
+  }
+
+  private static Activity createEmptyActivity(Activity activity) {
+    return new Activity()
+        .withType(ActivityTypes.MESSAGE)
+        .withRecipient(activity.from())
+        .withFrom(activity.recipient());
+  }
+
+}
