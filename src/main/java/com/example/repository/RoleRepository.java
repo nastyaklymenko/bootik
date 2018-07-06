cf6f6a7885468e60d2e52d27d6392ab8e4b194b1
Oleksii Dzhus <oleksa777@live.com>
2018-07-06 12:06:52 +0300

    User service created

--- src/main/java/com/example/repository/RoleRepository.java
+++ src/main/java/com/example/repository/RoleRepository.java
@@ -2,9 +2,8 @@ package com.example.repository;
 
 import com.example.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository("roleRepository")
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
   Role findByRole(String role);
--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.example.repository;
 
 import com.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
 
   User findByEmail(String email);
